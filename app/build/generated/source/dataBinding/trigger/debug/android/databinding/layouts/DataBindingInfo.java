6073767031cbfb9173b8ab5f57ec75228bdd8639
HerrPhil <phillip.j.wray.esq@gmail.com>
2018-07-02 23:10:18 -0600

    remove build files from repo

--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="c8a1f9d0-d4cb-49a6-ae2c-1ab6a591b071")
-public class DataBindingInfo {}
